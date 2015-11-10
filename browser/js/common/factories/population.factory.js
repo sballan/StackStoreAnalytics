de6dd8350b36cf9376471f0aef9d2cba5bcfe409
Will <willjacobson1@gmail.com>
2015-11-10 12:08:51 -0500

    going along

--- browser/js/common/factories/population.factory.js
+++ browser/js/common/factories/population.factory.js
@@ -1,21 +1,41 @@
 app.factory('PopulationFactory', function ($http) {
+	var usersCache = [];
+	var productsCache = [];
 
-	return {
+
+	PopulationFactory = {
 
 		getAllUsers: function() {
+			console.log('trying')
 			return $http.get('http://127.0.0.1:1337/api/users')
 			.then(function(res) {
+				usersCache = res.data;
 				return res.data;
 			});
 		},
 
 		getAllProducts: function() {
+			console.log('trying')
 			return $http.get('http://127.0.0.1:1337/api/products')
 			.then(function(res) {
+				productsCache = res.data;
 				return res.data;
 			});
-		}
+		},
+
+		// getParsedEntries: function() {
+		// 	console.log("This is the parsed entries function")
+		// 	this.getAllUsers()
+		// 	.then(function(users) {
+		// 		return this.getAllProducts()
+		// 	})
+		// 	.then(function(products) {
+		// 		return {users: usersCache, products: productsCache}
+		// 	})
+		// }
 
 	};
+
+	return PopulationFactory;
 
 });
--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,10 +1,24 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        resolve: {
+        	users: function(PopulationFactory) {
+        		return PopulationFactory.getAllUsers();
+        	},
+        	products: function(PopulationFactory) {
+        		return PopulationFactory.getAllProducts();
+        	}
+        },
+        controller: 'HomeCtrl'
     });
 });
 
-app.controller('HomeCtrl', function ($scope) {
+app.controller('HomeCtrl', function ($scope, users, products) {
+	console.log('hi');
+	$scope.users = users;
+	$scope.products = products;
 
+	console.log("users: ", $scope.users);
+	console.log("products: ", $scope.products);
 });
